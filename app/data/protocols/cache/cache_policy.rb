3de998b1b11646098cacfe461871ce305b3e1d7e
Andre Nascimento <contato.nascimentoandre@gmail.com>
2025-12-29 21:13:51 -0300

    feat: initial architecture

---
+++ app/data/protocols/cache/cache_policy.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+module Protocols
+  module Cache
+    class CachePolicy
+      MAX_AGE_IN_DAYS = 3
+
+      def self.validate(cache, current_date)
+        max_age = cache.timestamp + MAX_AGE_IN_DAYS
+
+        max_age > current_date
+      end
+    end
+  end
+end
---
+++ app/main/factories/usecases/local_load_purchases_factory.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../../../infra/cache/cache_store'
+require_relative '../../../data/usecases/load_purchases/local_load_purchases'
+
+module Factories
+  module UseCases
+    class LocalLoadPurchasesFactory
+      def self.build
+        cache_store = Cache::CacheStore
+        ::UseCases::LoadPurchases::LocalLoadPurchases.new(
+          cache_store:,
+          current_date: Date.today
+        )
+      end
+    end
+  end
+end
